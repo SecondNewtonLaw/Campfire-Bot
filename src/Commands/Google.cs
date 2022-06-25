d8bfae742f6825765503ee5c35b1d44c67c85876
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-06-25 15:46:58 -0300

    Definitive fix.

--- src/Commands/Google.cs
+++ src/Commands/Google.cs
@@ -17,29 +17,29 @@ internal partial class Commands
         const string BASE_URL = "https://www.google.com/search";
         await cmdSock.DeferAsync();
         string usrQuery = (string)cmdSock.Data.Options.ElementAt(0).Value;
-
-        #region Assemble URL
-
-        FormUrlEncodedContent encodedContent = new(
-            new List<KeyValuePair<string, string>>()
-            {
-                new KeyValuePair<string, string>("q", usrQuery)
-            });
-
-        StringBuilder assembler = new(BASE_URL);
-
-        assembler
-            .Append('?')
-            .Append(await encodedContent.ReadAsStringAsync());
-
-        #endregion Assemble URL
-
-
-
-        ScrapeGoogle scraper = new(assembler.ToString());
-
         try
         {
+            #region Assemble URL
+
+            FormUrlEncodedContent encodedContent = new(
+                new List<KeyValuePair<string, string>>()
+                {
+                new KeyValuePair<string, string>("q", usrQuery)
+                });
+
+            StringBuilder assembler = new(BASE_URL);
+
+            assembler
+                .Append('?')
+                .Append(await encodedContent.ReadAsStringAsync());
+
+            #endregion Assemble URL
+
+
+
+            ScrapeGoogle scraper = new(assembler.ToString());
+
+
             Task<List<ScrapedSearchResult>> scraperTask = scraper.GetResultsAsync();
 
             await scraperTask;
@@ -47,22 +47,32 @@ internal partial class Commands
             List<EmbedFieldBuilder> fields = new();
             List<ScrapedSearchResult> results = await scraperTask;
             results.OrderBy(x => x.ItemPosition); // Order elements
+            int addResults = 10;
 
-            for (int i = 0; i < 10; i++)
+            if (results.Count < 10) addResults = results.Count;
+
+            for (int i = 0; i < addResults; i++)
             {
                 fields.Add(new Discord.EmbedFieldBuilder() { Name = results[i].Title, Value = results[i].URL });
             }
             await cmdSock.FollowupAsync(embed: new EmbedBuilder() { Title = $"Searched \'{usrQuery}\' on google", Fields = fields }.Build());
         }
-        catch (NoResultsException)
+        catch (NoResultsException ex)
         {
+            Console.WriteLine(ex);
             await cmdSock.FollowupAsync(embed: new EmbedBuilder()
             {
                 Title = "No search results :(",
                 Description = $"No search results on google were found for query \'{usrQuery}\'",
                 Footer = Extensions.GetTimeFooter()
             }.Build());
+
             return;
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
         }
 
     }
